71318a1bf603b3514e7a9dbe90d86af32d1cd9b3
DELSAUT Pierre-arnaud <dlsprr@forem.be>
2024-05-22 17:11:59 +0200

    try to deploy static site

--- event-driven/docusaurus.config.ts
+++ event-driven/docusaurus.config.ts
@@ -39,8 +39,10 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
         },
         blog: false,
+        pages: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -79,7 +81,7 @@ const config: Config = {
           items: [
             {
               label: 'Synthèse',
-              to: '/docs/',
+              to: '/',
             },
           ],
         },
