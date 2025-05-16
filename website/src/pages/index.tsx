a134b6e762141c33d5724a208cce6cbeb36636a5
DELSAUT Pierre-arnaud <dlsprr@forem.be>
2025-05-16 09:26:48 +0200

    section profil

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -8,8 +8,30 @@ export default function PierreArnaudCV() {
       <div className={styles.page}>
         <div className={styles.container}>
           <div className={styles.columnLarge}>
-            <h2 className={styles.title}>Profil : Pierre-Arnaud Delsaut</h2>
+            <h2 className={styles.title}>Profil</h2>
             <div className={styles.titleLine}></div>
+            {/* Section Profil */}
+            <div className={styles.profileSection}>
+              <img
+                src="/img/profil.jpg"
+                alt="Pierre-Arnaud Delsaut"
+                className={styles.profilePhoto}
+              />
+              <div className={styles.profileDetails}>
+                <ul>
+                  <li>🎯Analyste-Développeur Full-Stack</li>
+                  <li>🎯Architecte Transversal-Solution-Intégration</li>
+                  <li>🛠️.NET / JS / SQL / Azure</li>
+                  <li>🏗️ Microservices / Event-Driven Architecture</li>
+                  <li>📦 Docker / Kubernetes</li>
+                  <li>♻️Agile / DevOps</li>
+                  <li>🗣️Français-Anglais-Néerlandais</li>
+                  <li>👉 + 15 ans d’expérience</li>
+                </ul>
+              </div>
+            </div>
+            {/* Fin Section Profil */}
+            <p></p>
           </div>
           <div className={styles.columnSmall}>
             <h2>Colonne 2</h2>
